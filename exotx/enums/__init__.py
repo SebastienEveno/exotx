6a9c70a5d44023d45ffa4b04cfd5feddfb69c7b9
Sébastien Eveno <sebastien.louis.eveno@gmail.com>
2023-09-29 19:51:40 +0200

    Add Vanilla option pricing to README.md

---
+++ exotx/enums/__init__.py
@@ -0,0 +1,10 @@
+"""Modules for working with exotic options.
+
+Exotic options can refer to auto-callables, barrier options, etc."""
+
+from exotx.enums.enums import PricingModel, NumericalMethod
+
+__all__ = [
+    'PricingModel',
+    'NumericalMethod'
+]
