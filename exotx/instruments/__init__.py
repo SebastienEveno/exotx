58cd6df21a306253c121bacf919135ecf550c9fc
Sébastien Eveno <72524233+SebastienEveno@users.noreply.github.com>
2023-09-29 20:32:07 +0200

    Update README.md (#35)

--- exotx/instruments/__init__.py
+++ exotx/instruments/__init__.py
@@ -3,12 +3,14 @@
 Exotic options can refer to auto-callables, barrier options, etc."""
 
 from exotx.instruments.autocallable import Autocallable
+from exotx.instruments.vanilla_option import VanillaOption
 from exotx.instruments.barrier_option import BarrierOption
 from exotx.instruments.instrument import price
 from exotx.instruments.option_type import OptionType
 
 __all__ = [
     'price',
+    'VanillaOption',
     'Autocallable',
     'BarrierOption',
     'OptionType'
